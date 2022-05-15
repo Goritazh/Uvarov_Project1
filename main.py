8e890e4f3461882b80c28ce0fe731139861f68f8
Arseny <arseny.uvaroff@gmail.com>
2022-05-15 22:05:16 +0300

    field

---
+++ main.py
@@ -0,0 +1,32 @@
+# импорты
+
+
+
+# глобальные переменные
+FIELD = [['']*3 for _ in range(3)]
+markers = ""
+
+# функции
+def field():
+    global FIELD
+    res = ""
+    for i in FIELD:
+        res += str(i) + "\n"
+    print(res)
+
+
+# чтение .ini файла
+
+
+# запуск суперцикла
+# while True:
+#     command = input()
+#
+#     if command in ('quit', 'выход'):
+#         # обработка завершения работы приложения
+#         break
+#
+#     # ввод имени игрока
+
+
+field()
