8dc34c5e20ddf2da6e0cb45f73c5efe0c087f116
Arseny <arseny.uvaroff@gmail.com>
2022-05-19 03:00:37 +0300

    работа с файлами ini (код с урока)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # импорты
-
-
+from configparser import ConfigParser
 
 # глобальные переменные
-FIELD = [['']*3 for _ in range(3)]
-markers = ""
+FIELD = [[''] * 3 for _ in range(3)]
+# markers = ""
+PLAYERS = {}
+SAVES = {}
+
 
 # функции
 def field():
@@ -14,18 +16,48 @@ def field():
         res += str(i) + "\n"
     print(res)
 
-# чтение .ini файла
 
+def show_help():
+    pass
+
+
+def read():
+    global PLAYERS, SAVES
+    config = ConfigParser()
+    if config.read('data.ini', encoding='utf-8'):
+        PLAYERS = {name.title(): [int(n) for n in score.split(',')]
+                   for name, score in config['Scores'].items()}
+        SAVES = {tuple(name.split(';')):
+                     [[' ' if c == '-' else c for c in field[i:i + 3]]
+                      for i in range(0, 9, 3)]
+                 for name, field in config['Saves'].items()}
+        return True if config['General']['first'] == 'yes' else False
+    else:
+        raise FileNotFoundError
+
+
+def save():
+    config = ConfigParser()
+    config['Scores'] = {name: ','.join(str(n) for n in score)
+                        for name, score in PLAYERS.items()}
+    config['Saves'] = {';'.join(name):
+                           ''.join(['-' if c == ' ' else c for r in field for c in r])
+                       for name, field in SAVES.items()}
+    config['General']['first'] = 'no'
+    with open('data.ini', 'w', encoding='utf-8') as config_file:
+        config.write(config_file)
+
+
+# чтение .ini файла
+if read():
+    show_help()
 
 # запуск суперцикла
-# while True:
-#     command = input()
-#
-#     if command in ('quit', 'выход'):
-#         # обработка завершения работы приложения
-#         break
-#
-#     # ввод имени игрока
+while True:
+    command = input()
 
+    if command in ('quit', 'выход'):
+        # обработка завершения работы приложения
+        break
 
-field()
+    # ввод имени игрока
